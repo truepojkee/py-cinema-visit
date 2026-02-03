f2c15e6877e5b11045cefd691696e77609a3e39b
Pojkee <cbo6oda22@gmail.com>
2026-02-03 15:26:54 +0200

    fixed print of sell_product method of CinemaBar and some other minor changes

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -6,4 +6,4 @@ class CinemaBar:
 
     @staticmethod
     def sell_product(customer: Customer, product: str) -> None:
-        print(f"Cinema bar sold {customer.food} to {customer.name}.")
+        print(f"Cinema bar sold {product} to {customer.name}.")
